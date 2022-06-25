be3aedfa0f2d0534a5f7a91b256f4f5a0a62c1f3
Max Baravy <maximkatut@gmail.com>
2022-06-25 09:29:23 -0400

    add assets: images

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,13 +7,15 @@ const Home: NextPage = () => {
       <Head>
         <title>Comments</title>
         <meta name="description" content="Interactive comments on NextJS" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/img/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
       </Head>
 
-      <main></main>
+      <main>
+        <div className="bg-red-200">Helloo</div>
+      </main>
 
       <footer></footer>
     </div>
