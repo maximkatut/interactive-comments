ba66ca07483dc07d9034c5663f14f7e962f7cc74
Max Baravy <maximkatut@gmail.com>
2022-07-08 16:38:24 -0400

    get user by name and by id

--- src/backend/router/user.ts
+++ src/backend/router/user.ts
@@ -2,13 +2,25 @@ import * as trpc from "@trpc/server";
 import { z } from "zod";
 import { prisma } from "db/client";
 
-export const userRouter = trpc.router().query("get", {
-  input: z.object({
-    userId: z.string(),
-  }),
-  async resolve({ input }) {
-    return await prisma.user.findFirst({
-      where: { id: input.userId },
-    });
-  },
-});
+export const userRouter = trpc
+  .router()
+  .query("getById", {
+    input: z.object({
+      userId: z.string(),
+    }),
+    async resolve({ input }) {
+      return await prisma.user.findFirst({
+        where: { id: input.userId },
+      });
+    },
+  })
+  .query("getByName", {
+    input: z.object({
+      name: z.string(),
+    }),
+    async resolve({ input }) {
+      return await prisma.user.findFirst({
+        where: { name: input.name },
+      });
+    },
+  });
