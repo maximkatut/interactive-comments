f6c56e71deecb32806dbc5d7670f9e6619cf3eb3
Max Baravy <maximkatut@gmail.com>
2022-07-08 16:01:49 -0400

    add ctx provider for nextauth and router with google and github providers

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,16 @@ import { withTRPC } from "@trpc/next";
 import superjson from "superjson";
 import { AppType } from "next/dist/shared/lib/utils";
 import { AppRouter } from "backend/router";
+import { SessionProvider } from "next-auth/react";
 
 import "../styles/globals.css";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const MyApp: AppType = ({ Component, pageProps: { session, ...pageProps } }) => {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 };
 
 export default withTRPC<AppRouter>({
---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,27 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  callbacks: {
+    async session({ session, token, user }) {
+      session.userId = user.id;
+      return session;
+    },
+  },
+});
