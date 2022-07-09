32c3e101ab937e8d82e68b7d216a5129cb019b6e
Max Baravy <maximkatut@gmail.com>
2022-07-09 11:28:53 -0400

    add auth secret

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -8,6 +8,7 @@ const prisma = new PrismaClient();
 
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
