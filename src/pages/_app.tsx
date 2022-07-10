f6b9f4241f3febf95cbb61679801354b344379a1
Max Baravy <maximkatut@gmail.com>
2022-07-10 11:44:03 -0400

    fix favicon path

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -24,5 +24,5 @@ export default withTRPC<AppRouter>({
       url,
     };
   },
-  ssr: true,
+  ssr: false,
 })(MyApp);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -55,9 +55,9 @@ const Home: NextPage = () => {
       <Head>
         <title>Comments</title>
         <meta name="description" content="Interactive comments on NextJS" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/img/favicon/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
       </Head>
       {modalIsShowed && deletingCommentId && <Modal commentId={deletingCommentId} />}
